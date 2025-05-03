496261db9a87998ebb24827d9aa2a06e80382145
Mahred1 <mredwan.kemal@gmail.com>
2025-05-03 14:56:37 +0300

    created the nav component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Homepage from "./pages/Homepage";
 import Signup from "./pages/Signup";
+import Nav from "./components/Nav";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Route index element={<Homepage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/test" element={<Nav />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ client/src/components/Nav.jsx
@@ -0,0 +1,30 @@
+import { Link, NavLink } from "react-router-dom";
+import Button from "./Button";
+
+function Nav() {
+  return (
+    <header className="w-[80%] m-auto mt-5">
+      <nav className="flex justify-between items-center">
+        <Link to={"/home"} id="logo">
+          <img width={170} src="logo-2.svg" alt="" />
+        </Link>
+        <div className="flex  gap-6 items-center">
+          <ul className="flex gap-5 text-[16.5px] text-[#232323]">
+            <li className="hover:text-primary transition ease-in">
+              <NavLink to={"/home"}>Home</NavLink>
+            </li>
+            <li className="hover:text-primary transition ease-in">
+              <NavLink to={"/recipes"}>Recipes</NavLink>
+            </li>
+            <li className="hover:text-primary transition ease-in">
+              <NavLink to={"/favourite"}>Favourite</NavLink>
+            </li>
+          </ul>
+          <button className="drop-shadow-md text-center transition ease-in font-light text-lg  text-white bg-primary border-1 border-primary px-5 py-1 rounded-[100px] hover:bg-white cursor-pointer hover:text-primary">Log out</button>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Nav;
