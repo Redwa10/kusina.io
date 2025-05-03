034f4d3bab366af800dee6f7b373ad068c5f8210
Mahred1 <mredwan.kemal@gmail.com>
2025-05-03 10:36:09 +0300

    created the basic layout for the login page with login route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Login from "./pages/Login"
 
 function App() {
 
   return <div>
-  hello
+  <BrowserRouter >
+  <Routes>
+    <Route path="/login" element={<Login/>}/>
+  </Routes>
+  </BrowserRouter>
 </div>
  
 }
