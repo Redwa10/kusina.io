896d1747fb5afd97e7bd46f5fee6b57c86072afc
Mahred1 <mredwan.kemal@gmail.com>
2025-05-04 12:40:52 +0300

    finished creating a filterable recipe gallery wwith data from the mock api

--- client/src/components/Category.jsx
+++ client/src/components/Category.jsx
@@ -1,6 +1,9 @@
-function Category({children,selected=false}) {
+function Category({children,onSetSelected,selected}) {
+function handleSelected(){
+    onSetSelected(children)
+}
     return (
-        <div className={`cursor-pointer ${selected?"bg-primary font-poppins w-fit px-8  py-1 rounded-4xl text-xl border-1 border-primary hover:bg-white hover:text-primary transition-colors ease-in ":"w-fit  font-poppins px-6  py-1 rounded-4xl text-[17px] border-1 border-[#9A9A9A] hover:bg-primary hover:text-white hover:border-transparent transition-colors ease-in"}`}>
+        <div onClick={handleSelected} className={`cursor-pointer ${selected?"bg-primary font-poppins w-fit px-8  py-1 rounded-4xl text-xl border-1 border-primary hover:bg-white hover:text-primary transition-colors ease-in ":"w-fit  font-poppins px-6  py-1 rounded-4xl text-[17px] border-1 border-[#9A9A9A] hover:bg-primary hover:text-white hover:border-transparent transition-colors ease-in"}`}>
            <p>{children}</p>
         </div>
     )
--- client/src/pages/Recipes.jsx
+++ client/src/pages/Recipes.jsx
@@ -1,18 +1,42 @@
+import { useEffect, useState } from "react";
 import Category from "../components/Category";
 import Nav from "../components/Nav";
+import { useRecipe } from "../contexts/RecipesContext";
+import Card from "../components/Card";
 
 function Recipes() {
+  const [selected, setSelected] = useState("All");
+  const { recipes} = useRecipe();
+  const [filtered, setFiltered] = useState([]);
+
+  useEffect(
+    function () {
+      if (selected === "All") {
+        setFiltered(recipes);
+        return;
+      }
+      setFiltered(recipes.filter((recipe) => recipe.category == selected));
+    },
+    [recipes, selected]
+  );
+
+  
   return (
     <main>
       <header>
         <Nav />
       </header>
       <section className="flex gap-5 justify-center mt-25">
-        <Category selected>All</Category>
-        <Category>Meat-based</Category>
-        <Category>Veg</Category>
-        <Category>Desert</Category>
-        <Category>Diary</Category>
+        <Category onSetSelected={setSelected}>All</Category>
+        <Category onSetSelected={setSelected}>Veg</Category>
+        <Category onSetSelected={setSelected}>Meat-based</Category>
+        <Category onSetSelected={setSelected}>Desert</Category>
+        <Category onSetSelected={setSelected}>Diary</Category>
+      </section>
+      <section className="grid grid-cols-4 justify-start justify-self-start gap-x-10 gap-y-14 mt-10 w-[80%] m-auto">
+        {filtered.map(recipe=>{
+            return <Card recipe={recipe} />
+        })}
       </section>
     </main>
   );
