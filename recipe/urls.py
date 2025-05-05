9c62b4674a6963f66576464da15672a214848e2b
Abdulhafiz redwan <hafizredwan10@gmail.com>
2025-05-05 08:55:50 +0100

    routes of recipe

--- recipe/urls.py
+++ recipe/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+#from rest_framework_nested import routers
 from . import views
+
+router = routers.DefaultRouter()
+router.register("recipes", views.RecipeViewSet)
 urlpatterns = [
-    path("recipes/", views.recipe)
+    path('', include(router.urls)),
 ]
