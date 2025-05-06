d7cb62d9bd61540edcce3f9b1ea469c72da1dbda
Mahred1 <mredwan.kemal@gmail.com>
2025-05-06 14:48:33 +0300

    finished creating an editor for admin to edit existing recipes

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -1,11 +1,61 @@
 import { Link } from "react-router-dom";
 import { useRecipe } from "../contexts/RecipesContext";
+import { useState } from "react";
+import EditRecipe from "../components/EditRecipe";
 
 function Admin() {
-  const { recipes, updateRecipes,isLoading } = useRecipe();
+  const { recipes, updateRecipes, isLoading } = useRecipe();
+  const [edit, setEdit] = useState(false);
+  const [currentRecipe, setCurrentRecipe] = useState(null);
+
   function handleDelete(id) {
     const updatedRecipe = recipes.filter((recipe) => recipe.id !== id);
     updateRecipes(updatedRecipe);
+  }
+
+  function AdminRecipe({ recipe }) {
+    return (
+      <div className="w-[80%] m-auto mt-15 flex justify-between font-poppins">
+        <div className="flex  items-center justify-between gap-3">
+          <div className="w-[230px] h-[150px]">
+            <img
+              className="w-full h-full object-cover object-center"
+              width={250}
+              src={`${recipe.imageUrl}`}
+              alt="food"
+            />
+          </div>
+          <div>
+            <p className="font-bold">{recipe.name}</p>
+            <p className="text-primary font-bold">
+              {recipe.ingredients.length}{" "}
+              <span className="text-[12px] font-normal text-black">
+                Ingredients
+              </span>
+            </p>
+          </div>
+        </div>
+        <div className="flex items-center justify-center gap-5">
+          <div
+            onClick={() => {
+              setEdit(!edit);
+              setCurrentRecipe(recipe);
+            }}
+            className="flex select-none  flex-col items-center cursor-pointer"
+          >
+            <img className="ml-2" width={30} src="edit.svg" alt="edit" />
+            <span className="text-[rgba(146, 139, 139, 1)]">edit</span>
+          </div>
+          <div
+            onClick={() => handleDelete(recipe.id)}
+            className="flex  select-none flex-col items-center cursor-pointer"
+          >
+            <img width={30} src="delete.svg" alt="delete" />
+            <span className="text-[rgba(146, 139, 139, 1)]">delete</span>
+          </div>
+        </div>
+      </div>
+    );
   }
   return (
     <main>
@@ -26,49 +76,12 @@ function Admin() {
       ) : (
         <section>
           {recipes.map((recipe) => {
-            return (
-              <div className="w-[80%] m-auto mt-15 flex justify-between font-poppins">
-                <div className="flex  items-center justify-between gap-3">
-                  <div className="w-[230px] h-[150px]">
-                    <img
-                      className="w-full h-full object-cover object-center"
-                      width={250}
-                      src={`${recipe.imageUrl}`}
-                      alt="food"
-                    />
-                  </div>
-                  <div>
-                    <p className="font-bold">{recipe.name}</p>
-                    <p className="text-primary font-bold">
-                      {recipe.ingredients.length}{" "}
-                      <span className="text-[12px] font-normal text-black">
-                        Ingredients
-                      </span>
-                    </p>
-                  </div>
-                </div>
-                <div className="flex items-center justify-center gap-5">
-                  <div className="flex  flex-col items-center cursor-pointer">
-                    <img
-                      className="ml-2"
-                      width={30}
-                      src="edit.svg"
-                      alt="edit"
-                    />
-                    <span className="text-[rgba(146, 139, 139, 1)]">edit</span>
-                  </div>
-                  <div onClick={()=>handleDelete(recipe.id)} className="flex  flex-col items-center cursor-pointer">
-                    <img width={30} src="delete.svg" alt="delete" />
-                    <span className="text-[rgba(146, 139, 139, 1)]">
-                      delete
-                    </span>
-                  </div>
-                </div>
-              </div>
-            );
+            return <AdminRecipe key={recipe.id} recipe={recipe} />;
           })}
         </section>
       )}
+
+      {edit && <EditRecipe onSetEdit={setEdit} currentRecipe={currentRecipe} />}
     </main>
   );
 }
