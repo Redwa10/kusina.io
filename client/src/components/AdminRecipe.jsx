aa638048e521018382fb18fe02038d3469999736
Mahred1 <mredwan.kemal@gmail.com>
2025-05-25 06:55:34 +0300

    integrated the delete endpoint for admin recipes

--- client/src/components/AdminRecipe.jsx
+++ client/src/components/AdminRecipe.jsx
@@ -1,56 +1,73 @@
 import { memo } from "react";
 import { useRecipe } from "../contexts/RecipesContext";
 
-const AdminRecipe= memo( function AdminRecipe({ recipe,setCurrentRecipe,onSetEdit }) {
-    const {recipes,updateRecipes} = useRecipe()
-    function handleDelete(id) {
-        const updatedRecipe = recipes.filter((recipe) => recipe.id !== id);
-        
-        updateRecipes(updatedRecipe);
+const AdminRecipe = memo(function AdminRecipe({
+  recipe,
+  setCurrentRecipe,
+  onSetEdit,
+}) {
+  const { recipes, updateRecipes } = useRecipe();
+  async function handleDelete(recipe) {
+    const res = await fetch(
+      `http://127.0.0.1:8000/recipebook/admin/recipes/${recipe.name.split(" ").join("-")}/`,
+      {
+        method: "DELETE",
+        headers: {
+          Authorization: `JWT ${localStorage.getItem("user")}`,
+        },
       }
-    
-    return (
-      <div className="w-[80%] m-auto mt-15 flex justify-between font-poppins">
-        <div className="flex  items-center justify-between gap-3">
-          <div className="w-[230px] h-[150px] rounded-lg overflow-hidden">
-            <img
-              className="w-full h-full object-cover object-center"
-              width={250}
-              src={`${recipe.imageUrls}`}
-              alt="food"
-            />
-          </div>
-          <div>
-            <p className="font-bold">{recipe.name}</p>
-            <p className="text-primary font-bold">
-              {recipe.ingredients.length}{" "}
-              <span className="text-[12px] font-normal text-black">
-                Ingredients
-              </span>
-            </p>
-          </div>
+    );
+
+    if (!res.ok) {
+      console.log(res);
+    } else {
+      const updatedRecipe = recipes.filter((myrecipe) => myrecipe.id !== recipe.id);
+
+      updateRecipes(updatedRecipe);
+    }
+  }
+
+  return (
+    <div className="w-[80%] m-auto mt-15 flex justify-between font-poppins">
+      <div className="flex  items-center justify-between gap-3">
+        <div className="w-[230px] h-[150px] rounded-lg overflow-hidden">
+          <img
+            className="w-full h-full object-cover object-center"
+            width={250}
+            src={`${recipe.imageUrls}`}
+            alt="food"
+          />
         </div>
-        <div className="flex items-center justify-center gap-5">
-          <div
-            onClick={() => {
-              onSetEdit(edit=>!edit);
-              setCurrentRecipe(recipe);
-            }}
-            className="flex select-none  flex-col items-center cursor-pointer"
-          >
-            <img className="ml-2" width={30} src="edit.svg" alt="edit" />
-            <span className="text-[rgba(146, 139, 139, 1)]">edit</span>
-          </div>
-          <div
-            onClick={() => handleDelete(recipe.id)}
-            className="flex  select-none flex-col items-center cursor-pointer"
-          >
-            <img width={30} src="delete.svg" alt="delete" />
-            <span className="text-[rgba(146, 139, 139, 1)]">delete</span>
-          </div>
+        <div>
+          <p className="font-bold">{recipe.name}</p>
+          <p className="text-primary font-bold">
+            {recipe.ingredients.length}{" "}
+            <span className="text-[12px] font-normal text-black">
+              Ingredients
+            </span>
+          </p>
         </div>
       </div>
-    );
-  }
-)
-  export default AdminRecipe
+      <div className="flex items-center justify-center gap-5">
+        <div
+          onClick={() => {
+            onSetEdit((edit) => !edit);
+            setCurrentRecipe(recipe);
+          }}
+          className="flex select-none  flex-col items-center cursor-pointer"
+        >
+          <img className="ml-2" width={30} src="edit.svg" alt="edit" />
+          <span className="text-[rgba(146, 139, 139, 1)]">edit</span>
+        </div>
+        <div
+          onClick={() => handleDelete(recipe)}
+          className="flex  select-none flex-col items-center cursor-pointer"
+        >
+          <img width={30} src="delete.svg" alt="delete" />
+          <span className="text-[rgba(146, 139, 139, 1)]">delete</span>
+        </div>
+      </div>
+    </div>
+  );
+});
+export default AdminRecipe;
