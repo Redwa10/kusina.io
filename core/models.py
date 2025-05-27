a47e676dacace9ab877216594e12bf89e8eec63d
Abdulhafiz redwan <hafizredwan10@gmail.com>
2025-05-28 00:01:12 +0100

    role

--- core/models.py
+++ core/models.py
@@ -3,4 +3,9 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
+    role_choices = [
+        ('admin', 'ADMIN'),
+        ('user', 'USER')
+    ]
+    role = models.CharField(max_length= 255, choices= role_choices, default= "USER")
     full_name = models.CharField(max_length= 255)
