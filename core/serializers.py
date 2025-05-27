0c8d5d9b6441ce851938fedf1522449f811625f1
Abdulhafiz redwan <hafizredwan10@gmail.com>
2025-05-27 23:27:37 +0100

    custom jwt

--- core/serializers.py
+++ core/serializers.py
@@ -1,6 +1,19 @@
 from rest_framework import serializers
-from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer
+from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer, TokenCreateSerializer
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+from core.models import User
 
 class UserCreateSerializer(BaseUserCreateSerializer):
     class Meta(BaseUserCreateSerializer.Meta):
-        fields = ['id','username', 'password', 'email', 'full_name']
+        fields = ['id','username', 'password', 'email', 'role' ,'full_name']
+
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token["full_name"] = user.full_name
+        token["role"] = user.role
+        return token
+
