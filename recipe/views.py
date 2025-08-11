6ed9d62e7ea9ae70beb06d48f28ca5920bbc4ce9
Abdulhafiz redwan <hafizredwan10@gmail.com>
2025-08-11 01:45:37 +0100

    b

--- recipe/views.py
+++ recipe/views.py
@@ -4,7 +4,7 @@ from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
 from rest_framework.decorators import api_view
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 from rest_framework.response import Response
-
+from rest_framework import status
 from core.models import User
 import recipe
 from .models import Catagories, Favorite, Recipe, Ingredient
@@ -27,13 +27,14 @@ class AdminRecipeViewSet(ModelViewSet):
 class FavoriteViewSet(ModelViewSet):
     serializer_class = FavoriteSerializers
     permission_classes = [IsAuthenticated]
+    lookup_field = 'id'
     def get_queryset(self):
         return Favorite.objects.filter(user = self.request.user)
     
     def perform_create(self, serializer):
         return serializer.save(user= self.request.user)
     
-
+   
 class CategoriesViewSet(ModelViewSet):
     serializer_class = CatagoriesSerializers
     queryset = Catagories.objects.all()
